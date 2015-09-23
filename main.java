7bc13880981934cb7c19e767738ace2d8bdac9f5
Carl-Henrik <ch.perssonn@gmail.com>
2015-09-23 16:27:13 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+package AdbServer;
+
+import java.sql.SQLException;
+
+public class main {
+
+	
+	public static void main(String args[]) throws SQLException{
+	
+		SQLHelper helper = new SQLHelper();
+		helper.getCityNets2();
+
+}
+}
